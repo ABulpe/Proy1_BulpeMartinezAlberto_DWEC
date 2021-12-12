98bb83e91745360272e08e1019c3e2f33ab2b93b
ABulpe <abulpe.alumno@iesrafaelalberti.es>
2021-12-12 22:33:34 +0100

    Añadida animación provisional para el 404 con sonido de fondo.

---
+++ js/login.js
@@ -0,0 +1,48 @@
+let users = [
+    {
+        username : "albertoDaw",
+        pass : "albertoDaw"
+    },
+    {
+        username: "beatrizDaw",
+        pass: "beatrizDaw"
+    },
+    {
+        username: "abrahamDaw",
+        pass: "abrahamDaw"
+    },
+    {
+        username: "manuelDaw",
+        pass: "manuelDaw"
+    }
+]
+
+
+
+//Inputs
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const button = document.querySelector(".form__button");
+
+
+
+//Output
+const output = document.createElement("span");
+output.setAttribute("class","error")
+
+
+const login_validate=(users,username,password)=>{ debugger;
+    let exist = false;
+    for(let user of users){
+        if(user.username === username.value && user.password === password) exist=true;
+    }
+    if(exist){
+        return true;
+
+    }else{
+        output.innerText = "*El usuario o la contraseña no coinciden";
+        button.insertAdjacentElement("afterend",output);
+        return false;
+    };
+}
+
